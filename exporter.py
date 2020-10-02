0ab9482c47de7c3cb8ce82d332a33edee317d205
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-10-02 11:23:59 +0100

    exporter.py: Add prometheus metrics and create the test func

--- exporter.py
+++ exporter.py
@@ -1,16 +1,33 @@
 import speedtest
 import datetime
+import time
+from prometheus_client import start_http_server, Summary, Gauge
 
 def btomb(speed):
     speed = speed * pow(10,-6)
     return speed
 
-servers = []
-s = speedtest.Speedtest()
-s.get_servers(servers)
-s.get_best_server()
-down_speed=s.download()
-up_speed=s.upload()
-currentDT = datetime.datetime.now()
-print("Current Download Speed:" + str(btomb(down_speed)) + " at: " + currentDT.strftime("%H:%M:%S"))
-print("Current Upload Speed:" + str(btomb(up_speed)) + " at: " + currentDT.strftime("%H:%M:%S"))
+ping = Gauge('speedtest_ping', 'Speedtest current Ping')
+download_speed = Gauge('speedtest_download_speed', 'Speedtest current Download Speed')
+upload_speed = Gauge('speedtest_upload_speed', 'Upload speed')
+
+def test():
+    servers = []
+    s = speedtest.Speedtest()
+    s.get_servers(servers)
+    s.get_best_server()
+    current_ping=s.results.ping
+    down_speed=s.download()
+    up_speed=s.upload()
+    currentDT = datetime.datetime.now()
+    print("Current Download Speed:" + str(btomb(down_speed)) + " at: " + currentDT.strftime("%H:%M:%S"))
+    print("Current Upload Speed:" + str(btomb(up_speed)) + " at: " + currentDT.strftime("%H:%M:%S"))
+    ping.set(current_ping)
+    download_speed.set(down_speed)
+    upload_speed.set(up_speed)
+    time.sleep(95)
+
+if __name__ == '__main__':
+    start_http_server(8000)
+    while True:
+        test()
