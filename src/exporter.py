99b2d744bec4b694df4154e08d46b7395d198b43
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-10-04 23:11:36 +0100

    exporter.py: Round the megabits result with 2 cases

--- src/exporter.py
+++ src/exporter.py
@@ -8,7 +8,7 @@ def bytes_to_bits(bytes_per_sec):
     return bytes_per_sec * 8
 
 def bits_to_megabits(bits_per_sec):
-    megabits = bits_per_sec * (10**-6)
+    megabits = round(bits_per_sec * (10**-6),2)
     return str(megabits) + " Mb/s"
 
 def is_json(myjson):
