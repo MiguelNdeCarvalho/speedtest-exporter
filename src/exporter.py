5cd1bf6b25d844e8c32ee37383c7439f33f7ab2e
Jan Souza <jangs@br.ibm.com>
2021-12-30 20:03:16 -0300

    Merge with last main code

--- src/exporter.py
+++ src/exporter.py
@@ -1,21 +1,41 @@
 import subprocess
 import json
 import os
+import logging
 from prometheus_client import make_wsgi_app, Gauge
 from flask import Flask
 from waitress import serve
-import logging
 
 app = Flask("Speedtest-Exporter")  # Create flask app
+
+
+# Setup logging values
+log_level_conf = {'debug': logging.DEBUG, 'info': logging.INFO,
+                  'warning': logging.WARNING, 'error': logging.ERROR}
+LOG_LEVEL = os.getenv('SPEEDTEST_LOG_LEVEL', 'info')
+
+format_string = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(encoding='utf-8', level=log_level_conf.get(LOG_LEVEL),
+                    format=format_string)
+
+
+# Disable Waitress Logs
+log = logging.getLogger('waitress')
+log.disabled = True
 
 # Create Metrics
 labels = ["server_id"]
 
-jitter = Gauge('speedtest_jitter_latency_milliseconds', 'Speedtest current Jitter in ms', labels)
-ping = Gauge('speedtest_ping_latency_milliseconds', 'Speedtest current Ping in ms', labels)
-download_speed = Gauge('speedtest_download_bits_per_second', 'Speedtest current Download Speed in bit/s', labels)
-upload_speed = Gauge('speedtest_upload_bits_per_second', 'Speedtest current Upload speed in bits/s', labels)
-packet_loss = Gauge('speedtest_packet_loss', 'Speedtest current Packet Loss in %', labels)
+jitter = Gauge('speedtest_jitter_latency_milliseconds',
+               'Speedtest current Jitter in ms', labels)
+ping = Gauge('speedtest_ping_latency_milliseconds',
+             'Speedtest current Ping in ms', labels)
+download_speed = Gauge('speedtest_download_bits_per_second',
+                       'Speedtest current Download Speed in bit/s', labels)
+upload_speed = Gauge('speedtest_upload_bits_per_second',
+                     'Speedtest current Upload speed in bits/s', labels)
+packet_loss = Gauge('speedtest_packet_loss',
+                    'Speedtest current Packet Loss in %', labels)
 up = Gauge('speedtest_up', 'Speedtest status whether the scrape worked', labels)
 
 
@@ -25,7 +45,7 @@ def bytes_to_bits(bytes_per_sec):
 
 def bits_to_megabits(bits_per_sec):
     megabits = round(bits_per_sec * (10**-6), 2)
-    return str(megabits) + " Mb/s"
+    return str(megabits) + "Mbps"
 
 
 def is_json(myjson):
@@ -44,34 +64,32 @@ def runTest():
            "--accept-license", "--accept-gdpr"]
     if serverID:
         cmd.append(f"--server-id={serverID}")
-
     try:
-        log.debug(cmd)
+        logging.debug(cmd)
         output = subprocess.check_output(cmd, timeout=timeout)
-
     except subprocess.CalledProcessError as e:
         output = e.output
         if not is_json(output):
             if len(output) > 0:
-                log.error('Speedtest CLI Error occurred that was not in JSON format')
-                log.error(output)
+                logging.error('Speedtest CLI Error occurred that' +
+                              'was not in JSON format')
             return (0, 0, 0, 0, 0, 0)
-
     except subprocess.TimeoutExpired:
-        log.error('Speedtest CLI process took too long to complete and was killed.')
+        logging.error('Speedtest CLI process took too long to complete ' +
+                      'and was killed.')
         return (0, 0, 0, 0, 0, 0)
 
     if is_json(output):
         data = json.loads(output)
-        log.debug(data)
+        logging.debug(data)
         if "error" in data:
             # Socket error
-            log.error('Something went wrong')
-            log.error(data['error'])
+            logging.error('Something went wrong')
+            logging.error(data['error'])
             return (0, 0, 0, 0, 0, 0)  # Return all data as 0
         if "type" in data:
             if data['type'] == 'log':
-                log.info(str(data["message"]))
+                logging.info(str(data["message"]))
             if data['type'] == 'result':
                 actual_server = data.get('server')
                 actual_jitter = data['ping']['jitter']
@@ -95,11 +113,12 @@ def updateResults():
     packet_loss.labels(server_id=r_server['id']).set(r_loss)
     up.labels(server_id=r_server['id']).set(r_status)
 
-    log.info("Server: " + str(r_server['id']) + " | Jitter: " + str(r_jitter)
-             + " ms | Ping: " + str(r_ping) + " ms | Download: "
-             + bits_to_megabits(r_download) + " | Upload:" + bits_to_megabits(r_upload)
-             + " | Packet Loss: " + str(r_loss))
-
+    logging.info("Server=" + str(r_server['id'])
+                 + " Jitter=" + str(r_jitter) + "ms"
+                 + " Ping=" + str(r_ping) + "ms"
+                 + " Download=" + bits_to_megabits(r_download)
+                 + " Upload=" + bits_to_megabits(r_upload)
+                 + " packetLoss=" + str(r_loss))
     return make_wsgi_app()
 
 
@@ -116,19 +135,6 @@ def mainPage():
 
 
 if __name__ == '__main__':
-
-    # Log Config
-    log_level_conf = {'debug': logging.DEBUG, 'info': logging.INFO, 'warning': logging.WARNING, 'error': logging.ERROR}
-    LOG_LEVEL = os.getenv('SPEEDTEST_LOG_LEVEL', 'info')
-
-    global log
-    log = logging.getLogger("speedtest-exporter")
-    log.setLevel(log_level_conf.get(LOG_LEVEL))
-    log.propagate = False
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    log.addHandler(handler)
-
     PORT = os.getenv('SPEEDTEST_PORT', 9798)
-    print("Starting Speedtest-Exporter on http://localhost:" + str(PORT))
+    logging.info("Starting Speedtest-Exporter on http://localhost:" + str(PORT))
     serve(app, host='0.0.0.0', port=PORT)
