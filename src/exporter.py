c4f08214b3a6473cb81eca8639aaf402de92fe23
Jason Kölker <jason@koelker.net>
2022-11-08 02:51:12 +0000

    feat(exporter): add result uuid label

--- src/exporter.py
+++ src/exporter.py
@@ -2,7 +2,10 @@ import subprocess
 import json
 import os
 import logging
+import dataclasses
 import datetime
+import uuid
+
 from prometheus_client import make_wsgi_app, Gauge
 from flask import Flask
 from waitress import serve
@@ -21,20 +24,51 @@ log = logging.getLogger('waitress')
 log.disabled = True
 
 # Create Metrics
-server = Gauge('speedtest_server_id', 'Speedtest server ID used to test')
-jitter = Gauge('speedtest_jitter_latency_milliseconds',
-               'Speedtest current Jitter in ms')
-ping = Gauge('speedtest_ping_latency_milliseconds',
-             'Speedtest current Ping in ms')
-download_speed = Gauge('speedtest_download_bits_per_second',
-                       'Speedtest current Download Speed in bit/s')
-upload_speed = Gauge('speedtest_upload_bits_per_second',
-                     'Speedtest current Upload speed in bits/s')
-up = Gauge('speedtest_up', 'Speedtest status whether the scrape worked')
+server = Gauge(
+    'speedtest_server_id',
+    'Speedtest server ID used to test',
+    labelnames=('result_uuid',),
+)
+jitter = Gauge(
+    'speedtest_jitter_latency_milliseconds',
+    'Speedtest current Jitter in ms',
+    labelnames=('result_uuid',),
+)
+ping = Gauge(
+    'speedtest_ping_latency_milliseconds',
+    'Speedtest current Ping in ms',
+    labelnames=('result_uuid',),
+)
+download_speed = Gauge(
+    'speedtest_download_bits_per_second',
+    'Speedtest current Download Speed in bit/s',
+    labelnames=('result_uuid',),
+)
+upload_speed = Gauge(
+    'speedtest_upload_bits_per_second',
+    'Speedtest current Upload speed in bits/s',
+    labelnames=('result_uuid',),
+)
+up = Gauge(
+    'speedtest_up',
+    'Speedtest status whether the scrape worked',
+    labelnames=('result_uuid',),
+)
 
 # Cache metrics for how long (seconds)?
 cache_seconds = int(os.environ.get('SPEEDTEST_CACHE_FOR', 0))
 cache_until = datetime.datetime.fromtimestamp(0)
+
+
+@dataclasses.dataclass
+class SpeedtestResult:
+    server: int
+    jitter: float
+    ping: float
+    download: float
+    upload: float
+    uuid: str
+    up: int
 
 
 def bytes_to_bits(bytes_per_sec):
@@ -55,6 +89,16 @@ def is_json(myjson):
 
 
 def runTest():
+    failed = SpeedtestResult(
+        server=0,
+        jitter=0,
+        ping=0,
+        download=0,
+        upload=0,
+        uuid=str(uuid.uuid4()),
+        up=0,
+    )
+
     serverID = os.environ.get('SPEEDTEST_SERVER')
     timeout = int(os.environ.get('SPEEDTEST_TIMEOUT', 90))
 
@@ -72,11 +116,11 @@ def runTest():
             if len(output) > 0:
                 logging.error('Speedtest CLI Error occurred that' +
                               'was not in JSON format')
-            return (0, 0, 0, 0, 0, 0)
+            return failed
     except subprocess.TimeoutExpired:
         logging.error('Speedtest CLI process took too long to complete ' +
                       'and was killed.')
-        return (0, 0, 0, 0, 0, 0)
+        return failed
 
     if is_json(output):
         data = json.loads(output)
@@ -84,18 +128,20 @@ def runTest():
             # Socket error
             print('Something went wrong')
             print(data['error'])
-            return (0, 0, 0, 0, 0, 0)  # Return all data as 0
+            return failed
         if "type" in data:
             if data['type'] == 'log':
                 print(str(data["timestamp"]) + " - " + str(data["message"]))
             if data['type'] == 'result':
-                actual_server = int(data['server']['id'])
-                actual_jitter = data['ping']['jitter']
-                actual_ping = data['ping']['latency']
-                download = bytes_to_bits(data['download']['bandwidth'])
-                upload = bytes_to_bits(data['upload']['bandwidth'])
-                return (actual_server, actual_jitter, actual_ping, download,
-                        upload, 1)
+                return SpeedtestResult(
+                    server=int(data['server']['id']),
+                    jitter=data['ping']['jitter'],
+                    ping=data['ping']['latency'],
+                    download=bytes_to_bits(data['download']['bandwidth']),
+                    upload=bytes_to_bits(data['upload']['bandwidth']),
+                    uuid=data.get('result', {}).get('id', str(uuid.uuid4())),
+                    up=1,
+                )
 
 
 @app.route("/metrics")
@@ -103,17 +149,22 @@ def updateResults():
     global cache_until
 
     if datetime.datetime.now() > cache_until:
-        r_server, r_jitter, r_ping, r_download, r_upload, r_status = runTest()
-        server.set(r_server)
-        jitter.set(r_jitter)
-        ping.set(r_ping)
-        download_speed.set(r_download)
-        upload_speed.set(r_upload)
-        up.set(r_status)
-        logging.info("Server=" + str(r_server) + " Jitter=" + str(r_jitter) +
-                     "ms" + " Ping=" + str(r_ping) + "ms" + " Download=" +
-                     bits_to_megabits(r_download) + " Upload=" +
-                     bits_to_megabits(r_upload))
+        result = runTest()
+        server.labels(result_uuid=result.uuid).set(result.server)
+        jitter.labels(result_uuid=result.uuid).set(result.jitter)
+        ping.labels(result_uuid=result.uuid).set(result.ping)
+        download_speed.labels(result_uuid=result.uuid).set(result.download)
+        upload_speed.labels(result_uuid=result.uuid).set(result.upload)
+        up.labels(result_uuid=result.uuid).set(result.up)
+
+        logging.info(
+            "Server=" + str(result.server) + " " +
+            "Jitter=" + str(result.jitter) + "ms " +
+            "Ping=" + str(result.ping) + "ms " +
+            "Download=" + bits_to_megabits(result.download) + " " +
+            "Upload=" + bits_to_megabits(result.upload) + " " +
+            "UUID=" + str(result.uuid)
+        )
 
         cache_until = datetime.datetime.now() + datetime.timedelta(
             seconds=cache_seconds)
